464c4bb171030a385cf397d55824d6278111a7e3
carmen <lunaesc85@gmail.com>
2025-02-06 11:16:32 -0400

    Guardando cambios en Practica styled-component

--- Practica styled-component/src/App.jsx
+++ Practica styled-component/src/App.jsx
@@ -1,33 +1,48 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+
+import styled, { createGlobalStyle, ThemeProvider } from "styled-components"
+
+
+const GlobalStyles = createGlobalStyle`
+  body{
+    background-color: #fff;
+    @import url('https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,100..900;1,100..900&display=swap');
+
+    *{
+      font-family: "Roboto", serif;
+    }
+  }
+`;
+
+const Container = styled.div`
+  background-color: #fff;
+  display: flex;
+  flex-direction: row;
+`;
+
+const Title = styled.h1`
+  color: ${(props) => props.theme.colors.secondary};
+`;
+
+const theme = {
+  colors: {
+    primary:"darkBlue",
+    secondary:"red",
+  },
+};
+
+
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+    <ThemeProvider theme={theme}>
+      <GlobalStyles/>
+      <Container>
+        <Title>
+          styled components #1
+        </Title>
+      </Container>
+    </ThemeProvider>
     </>
   )
 }
