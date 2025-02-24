7e06a68c1e54f81d580c304f45c18d68714abbdd
carmen <lunaesc85@gmail.com>
2025-02-24 14:45:33 -0400

    Practicas anteriores

--- Practica react-principiante-typeScript/src/Components/ListGroup.tsx
+++ Practica react-principiante-typeScript/src/Components/ListGroup.tsx
@@ -1,4 +1,6 @@
+import { useState } from "react";
 import styled from "styled-components"
+
 
 const ItemUl = styled.ul`
     list-style: none;
@@ -7,12 +9,13 @@ const ItemUl = styled.ul`
 `;
 
 
-const ListItemLi = styled.li`
+const ListItemLi = styled.li<{ isActive: boolean }>`
     position: relative;
     display: block;
     padding: 0%.75rem 1.25rem;
     margin-bottom: -1px;
-    background-color: #fff;
+    background-color: ${(props) => (props.isActive ? "#083b75" : "#0a54b4")};
+  color: ${(props) => (props.isActive ? "white" : "black")};
     border: 1px solid rgba(39, 22, 22, 0.125);
     transition: background-color 0.25s ease-in-out;
 
@@ -23,14 +26,23 @@ const ListItemLi = styled.li`
 `;
 
 function ListGroup() {
+const items = ["New York", "San Francisco", "Tokio", "London"]
+const [selectedIndex, setSelectedIndex] = useState(0);
+
   return (
     <>
         <ItemUl>
-            <ListItemLi >An item</ListItemLi>
-            <ListItemLi >A second item</ListItemLi>
-            <ListItemLi >A third item</ListItemLi>
-            <ListItemLi >A fourth item</ListItemLi>
-            <ListItemLi >And a fifth one</ListItemLi>
+            <h1>List</h1>
+            
+            {items.map((item, index) => (
+  <ListItemLi
+    key={index}
+    isActive={selectedIndex === index}
+    onClick={() => setSelectedIndex(index)}
+  >
+    {item}
+  </ListItemLi>
+))}
 
         </ItemUl>
     </>
